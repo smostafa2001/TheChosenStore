1ba36b4de1ef104a92d4c0fac4604bea7090aa4f
Mostafa Hosseini <s.mostafa.2001.1380@gmail.com>
2023-04-26 21:32:59 +0330

    Bug-Fix

--- Framework/Application/Email/EmailService.cs
+++ Framework/Application/Email/EmailService.cs
@@ -1,34 +1,34 @@
-﻿using MimeKit;
-using MailKit.Net.Smtp;
+﻿//using MimeKit;
+//using MailKit.Net.Smtp;
 
-namespace Framework.Application.Email
-{
-    public class EmailService : IEmailService
-    {
-        public void SendEmail(string title, string messageBody, string destination)
-        {
-            var message = new MimeMessage();
+//namespace Framework.Application.Email
+//{
+//    public class EmailService : IEmailService
+//    {
+//        public void SendEmail(string title, string messageBody, string destination)
+//        {
+//            var message = new MimeMessage();
 
-            var from = new MailboxAddress("Atriya", "test@atriya.com");
-            message.From.Add(from);
+//            var from = new MailboxAddress("Atriya", "test@atriya.com");
+//            message.From.Add(from);
 
-            var to = new MailboxAddress("User", destination);
-            message.To.Add(to);
+//            var to = new MailboxAddress("User", destination);
+//            message.To.Add(to);
 
-            message.Subject = title;
-            var bodyBuilder = new BodyBuilder
-            {
-                HtmlBody = $"<h1>{messageBody}</h1>",
-            };
+//            message.Subject = title;
+//            var bodyBuilder = new BodyBuilder
+//            {
+//                HtmlBody = $"<h1>{messageBody}</h1>",
+//            };
 
-            message.Body = bodyBuilder.ToMessageBody();
+//            message.Body = bodyBuilder.ToMessageBody();
 
-            var client = new SmtpClient();
-            client.Connect("185.88.152.251", 25, false);
-            client.Authenticate("test@atriya.com", "Atriya.123456");
-            client.Send(message);
-            client.Disconnect(true);
-            client.Dispose();
-        }
-    }
-}
+//            var client = new SmtpClient();
+//            client.Connect("185.88.152.251", 25, false);
+//            client.Authenticate("test@atriya.com", "Atriya.123456");
+//            client.Send(message);
+//            client.Disconnect(true);
+//            client.Dispose();
+//        }
+//    }
+//}
